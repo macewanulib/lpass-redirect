d753fc6077c553589038b474813f8b9e39d18ae7
Michael Schouten <schoutenm@macewan.ca>
2025-08-01 16:06:02 -0600

    [MS] - Readded the metadata script for easier registration

---
+++ metadata.php
@@ -0,0 +1,26 @@
+<?php
+
+  require_once '../vendor/autoload.php';
+
+  use OneLogin\Saml2\Auth;
+
+try {
+    $auth = new Auth();
+    $settings = $auth->getSettings();
+    $metadata = $settings->getSPMetadata();
+    $errors = $settings->validateMetadata($metadata);
+    if (empty($errors)) {
+        header('Content-Type: text/xml');
+        echo $metadata;
+    } else {
+        throw new OneLogin_Saml2_Error(
+            'Invalid SP metadata: '.implode(', ', $errors),
+            OneLogin_Saml2_Error::METADATA_SP_INVALID
+        );
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+
+?>
